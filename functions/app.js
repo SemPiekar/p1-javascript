4c14148f15f4caae492a1fb28f8ce87d212768b1
SemPiekar <9018659@student.zadkine.nl>
2022-09-15 16:19:57 +0200

    opdracht_7.1

---
+++ functions/app.js
@@ -0,0 +1,40 @@
+let num1;
+let num2;
+let answer;
+let answer2;
+let num3;
+let num4 = 5
+let num5;
+let num6;
+let answer3;
+let seconds;
+let minutes;
+
+const calculate = function(num1, num2) {
+    answer = num1 + num2
+    console.log(answer);
+}
+const multiplyByFive = function(num3) { 
+    answer2 = num3 * num4
+    console.log(answer2);
+}
+
+const checkInput = function (num5, num6) {
+    if (num5 > num6){
+        console.log(num5);
+    }
+    else {
+    console.log(num6);
+    }
+}
+
+const calculateMinutes = function (seconds) {
+    minutes = seconds / 60
+    console.log(minutes);
+}
+
+calculate(9,20);
+calculate(40,234182764123904781203498671243);
+multiplyByFive(5)
+checkInput(1,2)
+calculateMinutes(3600)
